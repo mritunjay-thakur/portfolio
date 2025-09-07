c1a6f4a2d72318bd3a368aeb3d9c0d9e93115ec4
Mriunjay Thakur (Jay) <162798050+mritunjay-thakur@users.noreply.github.com>
2025-09-08 00:44:01 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
